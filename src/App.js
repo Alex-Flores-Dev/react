0fbc07c24e7aad6afece6af7fb8676d0dc906daa
AlexDev <alexflores-ind@hotmail.com>
2023-07-24 18:12:58 -0400

    coc

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Cock
+          Learn React with Cock 2
         </a>
       </header>
     </div>
