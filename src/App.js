db764c5e09c11208fa18df9cc128ed5956ef279c
eddy-mb <62819123+emb-develop@users.noreply.github.com>
2023-08-10 18:50:06 -0400

    crj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./componentes/NavBar";
+import Inicio from "./componentes/inicio";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Inicio />
     </div>
   );
 }
